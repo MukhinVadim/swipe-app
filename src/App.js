a1af241a6be3f4de156cc69ee373b445ca0b7692
MukhinVadim <mukhinvadim@gmail.com>
2021-05-24 14:21:28 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from 'react'
+import './App.css'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.css'
+import Moment from "react-moment";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [value, setValue] = useState('')
+	const [values, setValues] = useState([])
+
+	const onChangeHandler = (event) => {
+		setValue(event.target.value)
+	}
+
+	const onClickHandler = () => {
+		setValues(prev => [...prev, value])
+		setValue('')
+	}
+
+	return (
+		<div className="App">
+			<Swiper
+				slidesPerView={ 1 }
+				pagination={ { clickable: true } }
+				onSwiper={ (swiper) => console.log(swiper) }
+				onSlideChange={ () => console.log('slide change') }
+			>
+				<SwiperSlide>
+					<div className="first-slide">
+						<div className="first-slide-form">
+							<textarea name="text"
+												placeholder="Enter your text here"
+												onChange={ onChangeHandler }
+												value={ value }
+												className="first-slide-form__textarea"/>
+							<button onClick={ onClickHandler }
+											className="first-slide-form__button">Send
+							</button>
+						</div>
+						{ values.map((value, index) =>
+							<div key={ index }
+									 className="first-slide__message">{ value }
+							</div>) }
+					</div>
+				</SwiperSlide>
+				<SwiperSlide>
+					<div className="second-slide">
+						<Moment format="HH:mm:ss" interval={ 1000 }/>
+					</div>
+				</SwiperSlide>
+			</Swiper>
+		</div>
+	);
 }
 
 export default App;
